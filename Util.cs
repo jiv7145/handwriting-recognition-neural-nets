b01e3ce08bc6dd275577bdfd662a91316a592765
Erik H <erik.624.h@gmail.com>
2020-04-16 23:17:19 -0700

    moved zip function to util

---
+++ Util.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Numpy;
+
+namespace WindowsFormsApp3
+{
+    class Util
+    {
+
+        public static List<Tuple<NDarray, NDarray>> zip(List<NDarray> darrays, NDarray ted1)
+        {
+
+            List<Tuple<NDarray, NDarray>> data = new List<Tuple<NDarray, NDarray>>();
+            for (int i = 0; i < darrays.Count; i++)
+            {
+                data.Add(new Tuple<NDarray, NDarray>(darrays[i], ted1[i]));
+            }
+            return data;
+        }
+
+        public static List<Tuple<NDarray, NDarray>> zip(List<NDarray> darrays, List<NDarray> ted1)
+        {
+            //Console.WriteLine(darrays.Count);
+            //Console.WriteLine(ted1.Count);
+            List<Tuple<NDarray, NDarray>> data = new List<Tuple<NDarray, NDarray>>();
+            for (int i = 0; i < darrays.Count; i++)
+            {
+                data.Add(new Tuple<NDarray, NDarray>(darrays[i], ted1[i]));
+            }
+            return data;
+        }
+    }
+}
--- loader.cs
+++ loader.cs
@@ -28,20 +28,20 @@ namespace WindowsFormsApp3
             for (int i = 0; i < tr_d[1].size; i++) {
                 training_results.Add(vectorized_result(tr_d[1][i]));
             }
-            List<Tuple<NDarray, NDarray>> training_data = zip(training_inputs, training_results);
+            List<Tuple<NDarray, NDarray>> training_data = Util.zip(training_inputs, training_results);
             //Console.WriteLine(te_d[0].size);
             //Console.WriteLine(va_d[0].size);
             List<NDarray> validation_inputs = new List<NDarray>();
             for (int i = 0; i < 10000; i++) {
                 validation_inputs.Add(np.reshape(va_d[0][i], new int[] {784, 1}));
             }
-            List<Tuple<NDarray, NDarray>> validation_data = zip(validation_inputs, va_d[1]);
+            List<Tuple<NDarray, NDarray>> validation_data = Util.zip(validation_inputs, va_d[1]);
 
             List<NDarray> test_inputs = new List<NDarray>();
             for (int i=0; i< 10000; i++) { //78400000/784 = 10000
                 test_inputs.Add(np.reshape(te_d[0][i], new int[] {784, 1}));
             }
-            List<Tuple<NDarray, NDarray>> test_data = zip(test_inputs, te_d[1]);
+            List<Tuple<NDarray, NDarray>> test_data = Util.zip(test_inputs, te_d[1]);
 
             List<List<Tuple<NDarray, NDarray>>> output = new List<List<Tuple<NDarray, NDarray>>>();
             output.Add(training_data);
@@ -50,29 +50,6 @@ namespace WindowsFormsApp3
 
             return output;
         }
-
-
-        private List<Tuple<NDarray, NDarray>> zip(List<NDarray> darrays, NDarray ted1) {
-
-            List<Tuple<NDarray, NDarray>> data = new List<Tuple<NDarray, NDarray>>();           
-            for (int i = 0; i < darrays.Count; i++) {
-                data.Add(new Tuple<NDarray, NDarray>(darrays[i], ted1[i]));
-            }
-            return data;
-        }
-
-        private List<Tuple<NDarray, NDarray>> zip(List<NDarray> darrays, List<NDarray> ted1) 
-        {
-            Console.WriteLine(darrays.Count);
-            Console.WriteLine(ted1.Count);
-            List<Tuple<NDarray, NDarray>> data = new List<Tuple<NDarray, NDarray>>();
-            for (int i = 0; i < darrays.Count; i++)
-            {
-                data.Add(new Tuple<NDarray, NDarray>(darrays[i], ted1[i]));
-            }
-            return data;
-        }
-
 
         private NDarray vectorized_result(NDarray darrays) {
             NDarray zz = np.zeros(new int[] {10, 1});
